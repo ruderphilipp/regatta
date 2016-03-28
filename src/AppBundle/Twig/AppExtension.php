eed6baf71204f6fdcf9b57b2059abd955446d514
Philipp Riemer <ruderphilipp@gmail.com>
2016-03-28 12:49:56 +0200

    add customized Twig extensions

---
+++ src/AppBundle/Twig/AppExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Twig;
+
+class AppExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('count', array($this, 'countFilter')),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions() {
+        return array(
+            new \Twig_SimpleFunction('sameDay', array($this, 'isSameDay')),
+        );
+    }
+
+    public function countFilter($countable)
+    {
+        return count($countable);
+    }
+
+    public function isSameDay(\DateTime $x, \DateTime $y) {
+        $days = $x->diff($y, true)->days;
+        return (0 == $days);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'app_extension';
+    }
+}

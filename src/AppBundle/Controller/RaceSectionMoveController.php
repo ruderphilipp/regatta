47f08985bf50742eb876931f03b6bfd7be9df0c9
Philipp Riemer <ruderphilipp@gmail.com>
2016-07-24 11:58:17 +0200

    Add option to move teams between sections
    If the team is the last one and is moved down, then it is added on lane 1
    of the next race section. If the team is the first one in its section and
    is moved up, then it is added on the last lane of the upper race section.
    
    See #33

--- src/AppBundle/Controller/RaceSectionMoveController.php
+++ src/AppBundle/Controller/RaceSectionMoveController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\RaceSection;
 use AppBundle\Entity\Registration;
+use Doctrine\ORM\NoResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -48,7 +49,18 @@ class RaceSectionMoveController extends Controller
         if ($registration->getLane() > 1) {
             return $this->moveInSection($request, $registration, 'up');
         } else {
-            return $this->addToNextSection($request, $registration, 'up');
+            $next = $this->getNextPossibleSection(
+                $registration,
+                function ($x, $y) {
+                    return $x < $y;
+                }
+            );
+            if(!is_null($next)) {
+                return $this->addToNextSection($request, $registration, 'up');
+            } else {
+                // TODO error message with flash and redirection
+                return new Response('Not possible', Response::HTTP_PRECONDITION_FAILED);
+            }
         }
     }
 
@@ -70,11 +82,27 @@ class RaceSectionMoveController extends Controller
             return new Response('Not possible', Response::HTTP_PRECONDITION_FAILED);
         }
 
-        // TODO check!
-        if (true) {
+        $section = $registration->getSection();
+        $myLane = $registration->getLane();
+
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:Registration');
+        $lastLane = $section->getRace()->getMaxStarterPerSection();
+
+        $next = $this->getNextPossibleSection(
+            $registration,
+            function ($x, $y) {
+                return $x > $y;
+            }
+        );
+
+        if (!($lastLane == $myLane)) {
             return $this->moveInSection($request, $registration, 'down');
-        } else {
+        } elseif(!is_null($next)) {
             return $this->addToNextSection($request, $registration, 'down');
+        } else {
+            // TODO error message with flash and redirection
+            return new Response('Not possible', Response::HTTP_PRECONDITION_FAILED);
         }
     }
 
@@ -95,7 +123,7 @@ class RaceSectionMoveController extends Controller
         }
         if(-1 == $myNewLane) {
             // TODO error message with flash and redirection
-            die('moveInSection');
+            die('[moveInSection] myNewLane = -1');
             // TODO remove this method as soon as preconditions check exists
         }
 
@@ -149,12 +177,84 @@ class RaceSectionMoveController extends Controller
 
         if (is_null($f)) {
             // TODO error message with flash and redirection
-            die('addToNextSection');
+            die('[addToNextSection] function is null');
             // TODO remove this method as soon as preconditions check exists
         }
 
-        $next = $this->getNextPossibleSection($registration, $f);
-        return new Response('Not implemented', Response::HTTP_NOT_IMPLEMENTED);
+        $newSection = $this->getNextPossibleSection($registration, $f);
+        if (is_null($newSection)) {
+            // TODO error message with flash and redirection
+            die('[addToNextSection] newSection is null');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:Registration');
+        $oldSection = $registration->getSection();
+        $oldLane = $registration->getLane();
+        $newLane = -1;
+
+        if ('up' == $direction) {
+            // add after the last currently taken lane
+            try {
+                $lastLane = $repo->createQueryBuilder('r')
+                    ->select('max(r.lane)')
+                    ->where('r.section = :section')
+                    ->setParameter('section', $newSection->getId())
+                    ->getQuery()
+                    ->getSingleScalarResult();
+            } catch (NoResultException $e) {
+                // TODO error message with flash and redirection
+                die('[addToNextSection] lastLane could not be determined');
+            }
+            $newLane = $lastLane + 1;
+
+            $registration->setSection($newSection);
+            $registration->setLane($newLane);
+        } elseif ('down' == $direction) {
+            // add as the first lane
+            $newLane = 1;
+
+
+            $other = null;
+            // search for the team that is currently on that position
+            /** @var Registration $team */
+            foreach ($newSection->getValidRegistrations() as $team) {
+                if ($team->getLane() == $newLane) {
+                    $other = $team;
+                    break;
+                }
+            }
+            if (is_null($other)) {
+                $registration->setSection($newSection);
+                $registration->setLane($newLane);
+            } else {
+                // if it exists, then move everyone plus one
+                foreach ($newSection->getValidRegistrations() as $team) {
+                    $team->setLane($team->getLane() + 1);
+                    $em->persist($team);
+                }
+                // then add the current one as the first
+                $registration->setSection($newSection);
+                $registration->setLane($newLane);
+            }
+
+        }
+        if(-1 == $newLane) {
+            // TODO error message with flash and redirection
+            die('moveInSection');
+            // TODO remove this method as soon as preconditions check exists
+        }
+
+        $em->persist($registration);
+        // store in DB
+        $em->flush();
+
+        $this->addFlash(
+            'notice',
+            "Sportler verschoben ({$oldSection->getNumber()}:{$oldLane} >> {$newSection->getNumber()}:{$newLane})"
+        );
+
+        return $this->redirect($request->headers->get('referer'));
     }
 
     /**
